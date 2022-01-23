2a4719661ea884b76a39298d8cb0fca8336c7a5c
PPrrooggrraammeerr <81397961+PPrrooggrraammeerr@users.noreply.github.com>
2022-01-23 15:38:29 -0300

    Add files via upload

---
+++ Administrator mode ENABLE.bat
@@ -0,0 +1,15 @@
+@echo off
+title Administrator mode ENABLE.bat
+color 0a
+mode con lines=19 cols=69
+goto :IFs
+:IFs
+cls
+timeout /t 0 > nul
+set Disable_Administrator="HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+for %%b in (%Disable_Administrator%) do (reg add %%b /v EnableLUA /t REG_DWORD /d 0x1)
+timeout /t 10 > nul
+cls
+goto :exit
+:exit
+exit
