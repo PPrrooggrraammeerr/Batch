5c4e80e22c8cabf8424ecd8d6fc09c552af985ae
PPrrooggrraammeerr <81397961+PPrrooggrraammeerr@users.noreply.github.com>
2022-04-08 10:41:44 -0300

    Update Wi-Fi, Change static IP, subnet mask and DNS.bat

--- Wi-Fi, Change static IP, subnet mask and DNS.bat
+++ Wi-Fi, Change static IP, subnet mask and DNS.bat
@@ -1,5 +1,5 @@
 @echo off
-title Change static IP, subnet mask and DNS.bat
+title Wi-Fi, Change static IP, subnet mask and DNS.bat
 color 0c
 mode con cols=105 lines=30
 goto :loop1
@@ -17,8 +17,8 @@ cls
 set /p Type_the_DNS="Type the DNS: "
 timeout /t 1 > nul
 cls
-goto :loop3
-:loop3
+goto :loop2
+:loop2
 cls
 for %%a in (%Type_the_static_IP%) do (netsh interface ipv4 set address name=Wi-Fi static %%a %Type_the_subnet_mask% %Type_the_Default_Gateway%) > nul
 timeout /t 1 > nul
@@ -26,6 +26,6 @@ cls
 for %%b in (%Type_the_DNS%) do (netsh interface ipv4 add dnsservers name=Wi-Fi %%b) > nul
 timeout /t 1 > nul
 cls
-goto :loop4
-:loop4
+goto :loop3
+:loop3
 exit
