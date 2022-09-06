2230dc5458a9369d1e02fa2a815fd96113086e41
PPrrooggrraammeerr <81397961+PPrrooggrraammeerr@users.noreply.github.com>
2022-09-06 16:29:35 -0300

    Add files via upload

---
+++ start program.bat
@@ -0,0 +1,15 @@
+@echo off
+title start program.bat
+color 0c
+mode con cols=65 lines=20
+:start_program
+cls
+timeout /t 0 > nul
+set /p start_program="start program: "
+timeout /t 0 > nul
+start %start_program%
+timeout /t 0 > nul
+cls
+goto: exit
+:exit
+exit
