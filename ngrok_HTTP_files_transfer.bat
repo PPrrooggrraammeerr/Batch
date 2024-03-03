a63ff1d86a86cdaaf73e9b978bf0ee3fe97f984b
Black coffee <81397961+PPrrooggrraammeerr@users.noreply.github.com>
2024-03-03 17:53:50 -0300

    Update ngrok_HTTP_files_transfer.bat

--- ngrok_HTTP_files_transfer.bat
+++ ngrok_HTTP_files_transfer.bat
@@ -4,15 +4,15 @@ color 0a
 mode con lines=20 cols=150
 goto :IFs_verify
 :IFs_verify
-if exists ("%userprofile%\path") goto :ngrok_files_transfer
-if not exists ("%userprofile%\path") goto :create_path
+if exist "%userprofile%\directory_HTTP" (goto :ngrok_files_transfer)
+if not exist "%userprofile%\directory_HTTP" (goto :create_path)
 :create_path
 cd %userprofile%
-mkdir path
+mkdir directory_HTTP
 goto :IFs_verify
 :ngrok_files_transfer
 cd %userprofile%
-set file="file:///%userprofile%\path"
+set file="file:///%userprofile%\directory_HTTP"
 for %%a in (%file%) do (call ngrok http %%a)
 cmd /k
 timeout /t 1 > nul
