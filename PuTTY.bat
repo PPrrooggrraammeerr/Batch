2d813d7074e9f141a215b330258cc0d82183b0cf
Black coffee <81397961+PPrrooggrraammeerr@users.noreply.github.com>
2025-03-23 00:25:49 -0300

    Update PuTTY.bat

--- PuTTY.bat
+++ PuTTY.bat
@@ -2,34 +2,19 @@
 title PuTTY.bat
 color 0c
 mode con cols=105 lines=30
-goto :PuTTY1.bat
-:PuTTY1.bat
-cls
-timeout /t 0 > nul
-for %%a in (%userprofile%) do (sc query sshd | find /i "sshd" >> %%a\sc_query_sshd.txt) > nul
-if exist "%userprofile%\sc_query_sshd.txt" (
-goto :PuTTY2.bat
-) else (goto :PuTTY4.bat)
-timeout /t 0 > nul
-cls
-goto :PuTTY2.bat
-:PuTTY2.bat
-cls
-timeout /t 0 > nul
-for /f "tokens=2 usebackq" %%b in (%userprofile%\sc_query_sshd.txt) do (sc start %%b) > nul
-timeout /t 0 > nul
-cls
-goto :PuTTY3.bat
-:PuTTY3.bat
-cls
-timeout /t 0 > nul
-set /p PuTTY1.bat="Host Name (or IP address): "
-timeout /t 1 > nul
-cls
-for %%c in (%PuTTY1.bat%) do (call ssh %%c)
+goto :get_service_sshd
+:get_service_sshd
+for %%a in (%userprofile%) do (sc query sshd | find /i "sshd" > %%a\sc_query_sshd.tmp) > nul
+if exist "%userprofile%\sc_query_sshd.tmp" (goto :read_service_sshd) else (goto :exit)
+goto :get_service_sshd
+:read_service_sshd
+for /f "tokens=2 usebackq" %%b in (%userprofile%\sc_query_sshd.tmp) do (sc start %%b) > nul
+goto :call_service_sshd
+:call_service_sshd
+set /p PuTTY="Host Name (or IP address): "
+cls & timeout /t 1 > nul
+for %%c in (%PuTTY%) do (call ssh %%c)
 cmd /k
-timeout /t 0 > nul
-cls
-goto :PuTTY4.bat
-:PuTTY4.bat
+goto :exit
+:exit
 exit
